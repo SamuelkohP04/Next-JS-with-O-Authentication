09af52c731d230ea282693843906828b8b126c9e
Sam <samuelkoh17@gmail.com>
2024-06-19 12:29:26 +0800

    chore: setting up route.ts handler for next-auth
    -  defining authorise function, as a handler object
    - with empty email and password fields

--- next-auth-tut/app/register/page.tsx
+++ next-auth-tut/app/register/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function RegisterPage() {
   return (
-    <form className="flex flex-col gap-2">
+    <form className="flex flex-col gap-2 mx-auto max-w-md mt-10 ">
       <input className="border" type="email" placeholder="Email" />
       <input className="border" type="password" placeholder="Password" />
       <button type="submit">Register</button>
