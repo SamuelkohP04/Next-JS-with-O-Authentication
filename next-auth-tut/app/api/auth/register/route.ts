f32a4ebae58f23f8c2c1cdd714f48ce6f2873767
Sam <samuelkoh17@gmail.com>
2024-06-19 13:38:47 +0800

    feat: onsubmit on register page function and its responses

--- next-auth-tut/app/api/auth/register/route.ts
+++ next-auth-tut/app/api/auth/register/route.ts
@@ -2,7 +2,6 @@ export default async function POST(request: Request) {
   try {
     const {email, password} = await request.json();
     //validate email and password
-    
     console.log(email, password);
   } catch(e) {
     console.log(e);
--- next-auth-tut/app/register/page.tsx
+++ next-auth-tut/app/register/page.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
 export default function RegisterPage() {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+    const formData = new FormData(e.currentTarget);
+    const response = await fetch('/api/auth/register', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: formData.get('email'),
+        password: formData.get('password'),
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    console.log(response);
+  };
+
   return (
-    <form className="flex flex-col gap-2 mx-auto max-w-md mt-10 ">
-      <input className="border" type="email" placeholder="Email" />
-      <input className="border" type="password" placeholder="Password" />
-      <button type="submit">Register</button>
+    <form onSubmit={handleSubmit} className="flex flex-col gap-2 mx-auto max-w-md mt-10">
+      <input className="border border-black" type="email" name="email" placeholder="Email" />
+      <input className="border border-black" type="password" name="password" placeholder="Password" />
+      <button type="submit" className="border border-black">Register</button>
     </form>
   );
 }
