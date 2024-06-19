81ae79301fd336a274a5e6df6d0f4d9a5ce818ff
Sam <samuelkoh17@gmail.com>
2024-06-19 14:21:44 +0800

    feat: using bcrypt to encrypt user passwords, and storing data in vercel postgresql

--- next-auth-tut/app/api/auth/register/route.ts
+++ next-auth-tut/app/api/auth/register/route.ts
@@ -1,14 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
+import {hash} from 'bcrypt';
+import {sql} from '@vercel/postgres'
 
 export async function POST(request: NextRequest) {
   try {
     const { email, password } = await request.json();
     // validate email and password
     console.log({ email, password });
-
+    const hashedPassword = await hash(password, 10);
     // Here you would typically handle registration logic,
     // such as saving the user to the database.
 
+    const response = await sql`
+    INSERT INTO users (email, password)
+    VALUES (${email}, ${hashedPassword})
+    `;
     return NextResponse.json({ message: "User registered successfully" });
   } catch (e) {
     console.log({ e });
